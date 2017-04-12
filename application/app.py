ccb6eae5eeb6ef5f9690ef4ec0a073d7d1f02639
Louis Willcock <louiswillcock@gmail.com>
2017-04-12 20:46:21 +1000

    minor changes to some env vars and whatnot, nothing major in this one, moving deeper into blueprints and jinja2 in the following commits

--- application/app.py
+++ application/app.py
@@ -5,11 +5,11 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
+    app.config.from_pyfile('settings.py', silent=True) #do not crash if .py doesnt exist
 
     @app.route('/')
     def index():
-        return "Hello, World?"
+        return app.config['HELLO']
 
     return app
 
--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,3 @@
-DEBUG = True
+DEBUG = True #detailed output, turn off in prod
+
+HELLO = 'Hello, World!'
